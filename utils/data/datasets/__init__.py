7fdaf2dc1ea80819bcffc633ac7e4e3040c834f7
结疤 <janglucky@163.com>
2019-08-26 21:55:25 +0800

    version 1.0.1

--- utils/data/datasets/__init__.py
+++ utils/data/datasets/__init__.py
@@ -2,7 +2,7 @@ from . import cuhk02
 from . import market1501
 
 __Factory__ = {
-	"cuhk02":cuhk02,
-	"market1501":market1501,
+	"cuhk02": cuhk02,
+	"market1501": market1501,
 }
 
---
+++ utils/data/datasets/market1501.py
@@ -0,0 +1,20 @@
+
+class market1501(object):
+    """
+        Dateset fold structure of  market1501:
+
+        -market1501
+            |
+            +- bounding_box_test
+            |
+            +- bounding_box_train
+            |
+            +- gt_bbox
+            |
+            +- gt_query
+            |
+            +- query
+
+    """
+    def __init__(self):
+        pass
