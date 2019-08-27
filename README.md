be437fc9e08999ad9bb38a0e1ab2b51efda21b54
结疤 <janglucky@163.com>
2019-08-27 14:59:45 +0800

    1.2

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
  深度学习行人重识别接口设计
 
 
- ##环境需求
+## 环境需求
  python3.x
  pytorch >= 1.0
  GPU support
--- __init__.py
+++ __init__.py
@@ -1,4 +1,2 @@
 from __future__ import absolute_import
 from __future__ import print_function
-
-import utils
